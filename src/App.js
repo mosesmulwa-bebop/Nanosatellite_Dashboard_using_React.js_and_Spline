c129d64e2fd8c915594ba42ad1916e53c505315c
Moses Mulwa <mosesmulwa123@gmail.com>
2022-06-26 17:01:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
+
+export default function App() {
+  return (
+    <Wrapper>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/9yBAq8hPQ3WD4Rg6/scene.splinecode"
+      />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  font-size: 16px;
+  color: white;
+  position: relative;
+
+  .spline {
+    position: absolute;
+  }
+`;
