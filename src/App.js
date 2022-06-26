7cf1842b90154f22c383226bf0cdabcae5b79e78
Moses Mulwa <mosesmulwa123@gmail.com>
2022-06-26 17:31:49 +0300

    added logos added logos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ export default function App() {
         className="spline"
         scene="https://prod.spline.design/9yBAq8hPQ3WD4Rg6/scene.splinecode"
       />
+      <Content>
+        <h1>Monitor nano-sats</h1>
+        <p>
+          Consolidate all your Nanosatellite information in one place. Monitor
+          status, send and receive telecommands on an easy to use dashboard.
+          Review received data. Join the private beta.
+        </p>
+      </Content>
     </Wrapper>
   );
 }
@@ -20,5 +28,34 @@ const Wrapper = styled.div`
 
   .spline {
     position: absolute;
+    margin: 0;
+    top: 0;
+    right: 0;
+  }
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 30px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+
+  h1 {
+    font-family: "Spline Sans Mono", monospace;
+    font-weight: bold;
+    font-size: 70px;
+    margin: 0;
+    max-width: 500px;
+  }
+  p {
+    font-weight: normal;
+    line-height: 150%;
+    max-width: 380px;
+  }
+  h1,
+  p {
+    margin: 0 30px 0 100px;
   }
 `;
