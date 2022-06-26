5f09c51c48b506f8d915a61a585b5e0abbb43be0
Moses Mulwa <mosesmulwa123@gmail.com>
2022-06-26 18:32:26 +0300

    Finished socials

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 import Logo from "./images/logo.svg";
 import Laptop from "./images/laptop2.svg";
-import LinkedIn from "./images/linkedin.svg";
-import Github from "./images/github.svg";
+import LinkedIn from "./images/linkedin2.svg";
+import Github from "./images/github2.svg";
 
 export default function App() {
   return (
@@ -14,9 +14,29 @@ export default function App() {
         scene="https://prod.spline.design/9yBAq8hPQ3WD4Rg6/scene.splinecode"
       />
       <Content>
+        <Menu>
+          <li>
+            <img src={Logo} alt="Logo" />
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Download</a>
+          </li>
+          <li>
+            <a href="/">App</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <button>Get Started</button>
+          </li>
+        </Menu>
         <h1>Monitor nano-sats</h1>
         <p>
-          Consolidate all your Nanosatellite information in one place. Monitor
+          Consolidate all your nanosatellite information in one place. Monitor
           status, send and receive telecommands on an easy to use dashboard.
           Review received data. <br />
           Join the private beta.
@@ -25,6 +45,11 @@ export default function App() {
           <img src={Laptop} alt="Download" /> Download for Windows
         </button>
       </Content>
+      <Social>
+        <div />
+        <img src={Github} alt="Github" />
+        <img src={LinkedIn} alt="LinkedIN" />
+      </Social>
     </Wrapper>
   );
 }
@@ -91,5 +116,52 @@ const Content = styled.div`
       border: 1px solid rgba(255, 255, 255, 0.8);
       transform: translateY(-3px);
     }
+  }
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  margin: 0 30px 0 100px;
+  padding: 0;
+
+  li {
+    list-style: none;
+    margin: 0;
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 20px;
+      border: 1px solid rgba(255, 255, 255, 0);
+      transition: 1s;
+      border-radius: 14px;
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+        transform: translateY(-3px);
+      }
+    }
+  }
+`;
+
+const Social = styled.div`
+  position: absolute;
+  top: 150px;
+  left: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+
+  div {
+    width: 1px;
+    height: 500px;
+    background: linear-gradient(
+      180deg,
+      #08b6f9 0%,
+      #6c56ef 33.57%,
+      #1306dd 65.86%,
+      #aa0eb2 100%
+    );
   }
 `;
